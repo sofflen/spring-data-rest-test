f7c71920b91bfd5041d18320eb1f5424c701d994
sofflen <nixxesgaming@gmail.com>
2024-08-27 18:13:51 +0300

    changed url path for Beer related requests and also a name of the returned collection

--- src/main/java/com/study/springdatarest/repositories/BeerRepository.java
+++ src/main/java/com/study/springdatarest/repositories/BeerRepository.java
@@ -5,10 +5,12 @@ import com.study.springdatarest.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 @Repository
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
